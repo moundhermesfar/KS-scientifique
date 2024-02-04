5238b0fe8c25d05c024d4ed4b1a96e1aa5a988d6
moundher mesfar <mesfarmounether@gmail.com>
2024-02-04 19:28:18 +0100

    cleaning the code

--- src/Components/Footer1.jsx
+++ src/Components/Footer1.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import logo from "../assets/logo-img.png";
 import { FaFacebookSquare, FaEnvelope, FaInstagram } from "react-icons/fa";
-// Reusable SocialIcon component with hover effect
+
 const SocialIcon = ({ icon: Icon }) => (
   <Icon
     className="social-icon hover:text-[#5abef3] hover:cursor-pointer"
     size={30}
   />
 );
-// Footer component
 const Footer1 = () => {
-  // Array defining the content and structure of the footer
   const items = [
-    // Social media icons
     { type: "icon", icon: FaFacebookSquare },
     { type: "icon", icon: FaEnvelope },
-    { type: 'icon', icon: FaInstagram },
-    // Footer sections
+    { type: "icon", icon: FaInstagram },
     {
       type: "section",
       title: "Appele-nous",
@@ -33,10 +28,8 @@ const Footer1 = () => {
       ],
     },
   ];
-  // JSX structure of the footer
   return (
-    <div className="lg:pr-[65px] lg:pl-10 pr-10 bg-[#141a63] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-10 text-gray-300">
-      {/* Left section with brand and social icons */}
+    <div className="lg:pr-[65px] lg:pl-10 pr-10 bg-[#141a63] mx-auto py-16 px-4 grid lg:grid-cols-2 gap-10 text-gray-300">
       <div>
         <h1 className="w-full text-3xl lg:text-4xl xl:text-5xl font-bold text-blue-500">
           <span className="text-red-500">KS </span>Scientifique
@@ -48,7 +41,6 @@ const Footer1 = () => {
           et du savoir.
         </p>
         <div className="flex justify-between md:w-[75%] my-6">
-          {/* Mapping over social icons and rendering the SocialIcon component */}
           {items.map((item, index) =>
             item.type === "icon" ? (
               <SocialIcon key={index} icon={item.icon} />
@@ -56,17 +48,14 @@ const Footer1 = () => {
           )}
         </div>
       </div>
-      {/* Right section with footer content organized in sections */}
-      <div className="lg:col-span-1 flex justify-between mt-6">
-        {/* Mapping over sections and rendering content */}
+      <div className="lg:col-span-1 flex mt-6">
         {items.map((item, index) =>
           item.title === "Entreprise" ? (
-            <div key={index}>
+            <div className="mx-[auto]" key={index}>
               <h6 className="font-medium py-3 text-gray-100 text-xl">
                 {item.title}
               </h6>
               <ul>
-                {/* Mapping over items in each section */}
                 {item.items.map((subItem, subIndex) => (
                   <li key={subIndex} className="py-2 text-sm">
                     <a href={`${subItem.link}`}>{subItem.title}</a>
@@ -75,12 +64,11 @@ const Footer1 = () => {
               </ul>
             </div>
           ) : item.title === "Appele-nous" ? (
-            <div key={index}>
+            <div className="mx-[auto]" key={index}>
               <h6 className="font-medium py-3 text-gray-100 text-xl">
                 {item.title}
               </h6>
               <ul>
-                {/* Mapping over items in each section */}
                 {item.items.map((subItem, subIndex) => (
                   <li key={subIndex} className="py-2 text-sm">
                     {subItem}
@@ -90,9 +78,6 @@ const Footer1 = () => {
             </div>
           ) : null
         )}
-      </div>
-      <div className="lg:col-span-1 flex justify-center items-center">
-        <img src={logo} alt="KS" />
       </div>
     </div>
   );
