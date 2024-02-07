6e856e0646dd5bad084d2ac5c43512d58efd3c10
moundher mesfar <mesfarmounether@gmail.com>
2024-02-07 12:09:31 +0100

    fixing the reason cards

--- src/Components/reasonCard.jsx
+++ src/Components/reasonCard.jsx
@@ -11,7 +11,7 @@ const ReasonCard = ({ reason }) => {
       variants={slideIn("left", "tween", 0, 0.5)}
       initial="hidden"
       animate={isVisibale ? "show" : ""}
-      className="p-3 max-h-[500px] h-[450px] bg-white  to-transparent rounded-[50px] px-4 py-2 m-4 relative text-center shadow-[3px_5px_5px_3px_#7EC8FF]"
+      className="p-3 max-h-[500px] h-[480px] bg-white  to-transparent rounded-[50px] px-4 py-2 m-4 relative text-center shadow-[3px_5px_5px_3px_#7EC8FF]"
     >
       <div className="mb-2 mx-auto">
         <img
@@ -23,7 +23,7 @@ const ReasonCard = ({ reason }) => {
         <h2 className="text-xl font-bold">{reason.title}</h2>
       </div>
       <div className="align-block align-middle flex items-center">
-        <p className="align-block align-middle max-h-[350px]">
+        <p className="align-block my-5 align-middle max-h-[350px]">
           {reason.paragraph}
         </p>
       </div>
